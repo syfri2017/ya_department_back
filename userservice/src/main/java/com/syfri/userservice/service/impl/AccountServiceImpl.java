63f38a130ddc17ee0005c192d2a343ea64b0bc0f
lixue0919 <442019696@qq.com>
2018-01-03 13:07:34 +0800

    新增账户表

--- userservice/src/main/java/com/syfri/userservice/service/impl/AccountServiceImpl.java
+++ userservice/src/main/java/com/syfri/userservice/service/impl/AccountServiceImpl.java
@@ -7,7 +7,6 @@ import org.apache.shiro.crypto.RandomNumberGenerator;
 import org.apache.shiro.crypto.SecureRandomNumberGenerator;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.util.ByteSource;
-import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
