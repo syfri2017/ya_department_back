dc8d3e497cff325f0cbb273bbbd987c707327dab
lixue0919 <442019696@qq.com>
2018-01-03 13:04:56 +0800

    新增账户表

---
+++ userservice/src/main/java/com/syfri/userservice/service/impl/AccountServiceImpl.java
@@ -0,0 +1,95 @@
+package com.syfri.userservice.service.impl;
+
+import com.syfri.userservice.model.AccountRoleVO;
+import com.syfri.userservice.model.RoleVO;
+import com.syfri.userservice.utils.CurrentUserUtil;
+import org.apache.shiro.crypto.RandomNumberGenerator;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.syfri.baseapi.service.impl.BaseServiceImpl;
+import com.syfri.userservice.dao.AccountDAO;
+import com.syfri.userservice.model.AccountVO;
+import com.syfri.userservice.service.AccountService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+@Service("accountService")
+public class AccountServiceImpl extends BaseServiceImpl<AccountVO> implements AccountService {
+
+	//指定加密算法为MD5
+	private String algorithmName = "MD5";
+
+	//指定加密迭代次数
+	private int hashIterations = 1;
+
+	@Autowired
+	private AccountDAO accountDAO;
+
+	@Override
+	public AccountDAO getBaseDAO() {
+		return accountDAO;
+	}
+
+	/*采用MD5对密码进行加密.*/
+	@Override
+	public AccountVO getPasswordEncry(AccountVO accountVO) {
+		RandomNumberGenerator randomNumberGenerator = new SecureRandomNumberGenerator();
+		accountVO.setSalt(randomNumberGenerator.nextBytes().toHex());
+		String newPassword = new SimpleHash(algorithmName, accountVO.getPassword(), ByteSource.Util.bytes(accountVO.getSalt()),hashIterations).toHex();
+		accountVO.setPassword(newPassword);
+		return accountVO;
+	}
+
+	/*新增：向账户表中增加账户.*/
+	@Override
+	public int doInsertAccountByVO(AccountVO accountVO){
+		accountVO.setCreateName(CurrentUserUtil.getCurrentUserName());
+		accountVO.setCreateId(CurrentUserUtil.getCurrentUserId());
+		accountVO = this.getPasswordEncry(accountVO);
+		//accountVO = ((AccountService) AopContext.currentProxy()).getPasswordEncry(accountVO);
+		return accountDAO.doInsertByVO(accountVO);
+	}
+
+	/*修改：修改账户表*/
+	@Override
+	public int doUpdateAccountByVO(AccountVO accountVO){
+		accountVO.setAlterName(CurrentUserUtil.getCurrentUserName());
+		accountVO.setAlterId(CurrentUserUtil.getCurrentUserId());
+		accountVO = this.getPasswordEncry(accountVO);
+		//accountVO = ((AccountService) AopContext.currentProxy()).getPasswordEncry(accountVO);
+		return accountDAO.doUpdateByVO(accountVO);
+	}
+
+	/*向用户角色中间表批量插入数据.*/
+	@Override
+	public int doInsertAccountRolesBatch(String userid, List<RoleVO> roles){
+		List<AccountRoleVO> accountRoles = new ArrayList<>();
+		if(roles!=null && roles.size()>0){
+			for(RoleVO role : roles){
+				AccountRoleVO temp = new AccountRoleVO();
+				temp.setUserid(userid);
+				temp.setRoleid(role.getRoleid());
+				temp.setCreateId(CurrentUserUtil.getCurrentUserId());
+				temp.setCreateName(CurrentUserUtil.getCurrentUserName());
+				accountRoles.add(temp);
+			}
+			return accountDAO.doBatchInsertAccountRoles(accountRoles);
+		}else{
+			return 0;
+		}
+	}
+
+	/*--删除：删除账户同时删除其角色(中间表).--*/
+	public int doDeleteAccountRoles(String userid){
+		return accountDAO.doDeleteAccountRoles(userid);
+	}
+
+}
