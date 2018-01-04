5e435247a65cdbba4f53502e513ca6edfae102be
lixue0919 <442019696@qq.com>
2018-01-04 16:26:37 +0800

    修改注释文字

--- userservice/src/main/java/com/syfri/userservice/dao/RoleDAO.java
+++ userservice/src/main/java/com/syfri/userservice/dao/RoleDAO.java
@@ -7,28 +7,18 @@ import java.util.List;
 
 public interface RoleDAO extends BaseDAO<RoleVO>{
 
-	/*根据用户取角色.*/
+	/*--根据用户取角色.--*/
 	List<RoleVO> doFindRoleByUserid(String userid);
 
-	/*获取权限，如果roleVO为null，则获取所有权限.*/
-	List<RoleVO> doFindRolePermissions(RoleVO roleVO);
+	/*--获取角色，如果roleVO为null，则获取所有角色.--*/
+	List<RoleVO> doFindRoleResources(RoleVO roleVO);
 
-	/*新增角色时批量插入用户权限.*/
-	int doBatchInsertRolePermissions(List<RolePermissionVO> rolePermissions);
+	/*--新增角色时批量插入用户资源(中间表).--*/
+	int doInsertRoleResourcesBatch(List<RoleResourceVO> roleResources);
 
-	/*修改角色时先删除角色权限中间表数据*/
-	int doDeleteRolePermissions(String roleid);
+	/*--修改角色时先删除角色资源数据(中间表).--*/
+	int doDeleteRoleResources(String roleid);
 
-	/*修改角色时修改用户中间表数据*/
-	int doBatchDeleteRolePermissions(String roleid);
-
-	/*新增：角色菜单时批量插入中间表.*/
-	int doBatchInsertRoleMenus(List<RoleMenuVO> roleMenus);
-
-	/*修改：角色菜单时先删除中间表相应数据*/
-	int doDeleteRoleMenus(String roleid);
-
-	/*删除角色时同时删除菜单中间表*/
-	int doBatchDeleteRoleMenus(String roleid);
-
+	/*--修改角色时修改用户中间表数据--*/
+	int doDeleteRoleResourcesBatch(String roleid);
 }
--- userservice/src/main/java/com/syfri/userservice/dao/UserDAO.java
+++ userservice/src/main/java/com/syfri/userservice/dao/UserDAO.java
@@ -1,7 +1,7 @@
 package com.syfri.userservice.dao;
 
 import com.syfri.baseapi.dao.BaseDAO;
-import com.syfri.userservice.model.UserRoleVO;
+import com.syfri.userservice.model.AccountRoleVO;
 import com.syfri.userservice.model.UserVO;
 
 import java.util.List;
@@ -10,14 +10,5 @@ public interface UserDAO extends BaseDAO<UserVO>{
 
 	/*获取用户，如果userVO为null，则获取所有用户.*/
 	List<UserVO> doFindUserRoles(UserVO userVO);
-
-	/*新增用户时批量插入用户角色.*/
-	int doBatchInsertUserRoles(List<UserRoleVO> userRoles);
-
-	/*修改用户时删除用户中间表数据*/
-	int doDeleteUserRoles(String userid);
-
-	/*修改用户时修改用户中间表数据*/
-	int doBatchDeleteUserRoles(String userid);
 
 }
