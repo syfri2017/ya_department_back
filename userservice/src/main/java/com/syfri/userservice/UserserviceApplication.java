51914ba63e2b93288c86237db05cd7f6d187c7d9
lixue0919 <442019696@qq.com>
2018-01-04 16:04:14 +0800

    增加事务代理

--- userservice/src/main/java/com/syfri/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/syfri/userservice/UserserviceApplication.java
@@ -3,12 +3,15 @@ package com.syfri.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
 @SpringBootApplication
-@EnableTransactionManagement
 @ServletComponentScan
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@EnableTransactionManagement
 public class UserserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
