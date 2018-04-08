040597b1d22b8a18ed4218a91ef877c7213ed47f
Yushuchen <1486851958@qq.com>
2018-04-08 11:06:58 +0700

    消火栓表结构改变

--- digitalplan/src/main/java/com/syfri/digitalplan/model/basicinfo/watersource/XhsVO.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/model/basicinfo/watersource/XhsVO.java
@@ -8,38 +8,26 @@ public class XhsVO extends ValueObject implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
-	private String pkid;	//PKID
-	private String id;	//UUID
-	private String gwid;	//所属管网
-	private String szxs;	//设置形式
-	private String gsdw;	//供水单位
-	private String lxfs;	//联系方式
-	private String gwxs;	//管网形式
-	private String gwzj;	//管网直径
-	private String gwyl;	//管网压力
-	private String jkxs;	//接口形式
-	private String ll;	//流量
-	private String jgid;	//机构ID
-	private String jlzt;	//记录状态
-	private String cszt;	//传输状态
-	private String sjbb;	//数据版本
-	private String cjsj;	//创建时间
-	private String ywxtbsid;	//业务系统部署ID
-	private String sjc;	//时间戳
-	private String bz;	//备注
-	private String vercol;	//版本戳
-	private String tbjdh;	//同步节点号
-	private String tbsjc;	//同步时间戳
-	private String reserve1;	//备用字段1
-	private String reserve2;	//备用字段2
-	private String reserve3;	//备用字段3
+	private String id;	//undefined
+	private String gwid;	//undefined
+	private String szxs;	//undefined
+	private String gsdw;	//undefined
+	private String lxfs;	//undefined
+	private String gwxs;	//undefined
+	private String gwzj;	//undefined
+	private String gwyl;	//undefined
+	private String jkxs;	//undefined
+	private String ll;	//undefined
+	private String jgid;	//undefined
+	private String jlzt;	//undefined
+	private String cszt;	//undefined
+	private String sjbb;	//undefined
+	private String cjsj;	//undefined
+	private String ywxtbsid;	//undefined
+	private String sjc;	//undefined
+	private String bz;	//undefined
+	private String vercol;	//undefined
 
-	public String getPkid(){
-		return pkid;
-	}
-	public void setPkid(String pkid){
-		this.pkid = pkid;
-	}
 	public String getId(){
 		return id;
 	}
@@ -153,35 +141,5 @@ public class XhsVO extends ValueObject implements Serializable{
 	}
 	public void setVercol(String vercol){
 		this.vercol = vercol;
-	}
-	public String getTbjdh(){
-		return tbjdh;
-	}
-	public void setTbjdh(String tbjdh){
-		this.tbjdh = tbjdh;
-	}
-	public String getTbsjc(){
-		return tbsjc;
-	}
-	public void setTbsjc(String tbsjc){
-		this.tbsjc = tbsjc;
-	}
-	public String getReserve1(){
-		return reserve1;
-	}
-	public void setReserve1(String reserve1){
-		this.reserve1 = reserve1;
-	}
-	public String getReserve2(){
-		return reserve2;
-	}
-	public void setReserve2(String reserve2){
-		this.reserve2 = reserve2;
-	}
-	public String getReserve3(){
-		return reserve3;
-	}
-	public void setReserve3(String reserve3){
-		this.reserve3 = reserve3;
 	}
 }
