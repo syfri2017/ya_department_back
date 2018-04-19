0b2acb4b0f61f181ab58c46345c23e4266299fcd
huang-rui <hr18904047625@163.com>
2018-04-19 14:14:07 +0800

    初版

---
+++ digitalplan/src/main/java/com/syfri/digitalplan/dao/digitalplan/OtherobjectsplanDAO.java
@@ -0,0 +1,9 @@
+package com.syfri.digitalplan.dao.digitalplan;
+
+import com.syfri.baseapi.dao.BaseDAO;
+import com.syfri.digitalplan.model.digitalplan.OtherobjectsplanVO;
+
+import java.util.List;
+
+public interface OtherobjectsplanDAO extends BaseDAO<OtherobjectsplanVO>{
+}
---
+++ digitalplan/src/main/java/com/syfri/digitalplan/service/digitalplan/OtherobjectsplanService.java
@@ -0,0 +1,9 @@
+package com.syfri.digitalplan.service.digitalplan;
+
+import com.syfri.baseapi.service.BaseService;
+import com.syfri.digitalplan.model.digitalplan.OtherobjectsplanVO;
+
+import java.util.List;
+
+public interface OtherobjectsplanService  extends BaseService<OtherobjectsplanVO>{
+}
