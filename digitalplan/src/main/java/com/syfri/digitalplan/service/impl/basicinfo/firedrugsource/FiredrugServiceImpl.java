865095af8b5131b9c82828c5c4dd114b253d8cdb
rliu666 <rliu666@sina.cn>
2018-04-22 19:34:35 +0800

    消防药剂模块药剂类型级联选择器和消防队站查询修改

--- digitalplan/src/main/java/com/syfri/digitalplan/service/impl/basicinfo/firedrugsource/FiredrugServiceImpl.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/service/impl/basicinfo/firedrugsource/FiredrugServiceImpl.java
@@ -23,17 +23,45 @@ public class FiredrugServiceImpl extends BaseServiceImpl<FiredrugVO> implements 
 		return firedrugDAO;
 	}
 
-	/*--条件查询：重点单位.--*/
+	/**
+	 * @Description:条件查询：消防药剂
+	 * @Param: [firedrugVO]
+	 * @Return: java.util.List<com.syfri.digitalplan.model.basicinfo.firedrugsource.FiredrugVO>
+	 * @Author: liurui
+	 * @Modified By:
+	 * @Date: 2018/4/20 16:55
+	 */
 	@Override
 	public List<FiredrugVO> doFindlist(FiredrugVO firedrugVO){
 		//所属队站
 		if(firedrugVO.getSsdz()!=null && !"".equals(firedrugVO.getSsdz())){
-			firedrugVO.setSsdz(firedrugVO.getSsdz().toUpperCase());
+			String str = firedrugVO.getSsdz().substring(0,2).toString();
+			firedrugVO.setSsdz(str);
 		}
 		//药剂类型
 		if(firedrugVO.getYjlx()!=null && !"".equals(firedrugVO.getYjlx())){
-			firedrugVO.setYjlx(firedrugVO.getYjlx().toUpperCase());
+			firedrugVO.setYjlx(this.delZero(firedrugVO.getYjlx()));
 		}
 		return firedrugDAO.doSearchByVO(firedrugVO);
+	}
+
+	public String delZero(String tempString) {
+		int initlen = tempString.length(); // 串的初始长度
+		int finallen = initlen; // 串的最终长度
+		int start = 0; // 串的开始位置
+		int off = 0; // 串的偏移位置
+		char[] val = new char[initlen];
+		tempString.getChars(0, finallen, val, 0); // 保存原数据，用于判断字符
+
+		// 找到以'0'开头的后一位
+		// while ((start < finallen) && (val[off + start] == '0')) {
+		// start++;
+		// }
+		// 找到以'0'结尾的前一位
+		while ((start < finallen) && (val[off + finallen - 1] == '0')) {
+			finallen--;
+		}
+		return ((start > 0) || (finallen < initlen)) ? tempString.substring(start, finallen)
+				: tempString;
 	}
 }
