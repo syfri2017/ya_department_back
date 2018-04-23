40c733475fbadf30def5d53dce156669ac03ea95
1106612528 <1106612528@qq.com>
2018-04-23 10:30:49 +0800

    上传附件后台代码

--- digitalplan/src/main/java/com/syfri/digitalplan/dao/yafjxz/YafjxzDAO.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/dao/yafjxz/YafjxzDAO.java
@@ -3,6 +3,8 @@ package com.syfri.digitalplan.dao.yafjxz;
 import com.syfri.baseapi.dao.BaseDAO;
 import com.syfri.digitalplan.model.yafjxz.YafjxzVO;
 
+import java.util.List;
+
 public interface YafjxzDAO extends BaseDAO<YafjxzVO>{
     public void doDeletcNotIn(String oldYafjxzVOs);
 }
