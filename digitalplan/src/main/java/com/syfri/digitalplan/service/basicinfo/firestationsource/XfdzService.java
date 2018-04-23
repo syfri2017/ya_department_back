9eacd2581f309be08006d80e856936202ed2182a
Yushuchen <1486851958@qq.com>
2018-04-23 20:23:14 +0800

    消防队站

---
+++ digitalplan/src/main/java/com/syfri/digitalplan/service/basicinfo/firestationsource/XfdzService.java
@@ -0,0 +1,7 @@
+package com.syfri.digitalplan.service.basicinfo.firestationsource;
+
+import com.syfri.baseapi.service.BaseService;
+import com.syfri.digitalplan.model.basicinfo.firestationsource.XfdzVO;
+
+public interface XfdzService  extends BaseService<XfdzVO>{
+}
