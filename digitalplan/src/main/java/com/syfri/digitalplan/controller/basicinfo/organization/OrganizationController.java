70c3d0772514c360f1ffb067ef6601a1d716829c
JC2177985 <14690588@qq.com>
2018-04-27 09:58:45 +0800

    消防组织机构迁移

--- digitalplan/src/main/java/com/syfri/digitalplan/controller/basicinfo/organization/OrganizationController.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/controller/basicinfo/organization/OrganizationController.java
@@ -1,26 +1,22 @@
-package com.syfri.digitalplan.controller.digitalplan;
+package com.syfri.digitalplan.controller.basicinfo.organization;
+import com.syfri.baseapi.controller.BaseController;
 import com.syfri.baseapi.model.ResultVO;
 import com.syfri.baseapi.utils.EConstants;
-import com.syfri.digitalplan.model.digitalplan.OrganizationVO;
+import com.syfri.digitalplan.model.basicinfo.organization.OrganizationVO;
+import com.syfri.digitalplan.service.basicinfo.organization.OrganizationService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.core.env.Environment;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import com.syfri.digitalplan.service.digitalplan.OrganizationService;
-import com.syfri.baseapi.controller.BaseController;
-
-import java.util.List;
 
 @Api(value = "组织机构",tags = "组织机构",description = "组织机构")
 @RestController
 @RequestMapping("organization")
-public class OrganizationController  extends BaseController<OrganizationVO>{
+public class OrganizationController extends BaseController<OrganizationVO>{
 
 	private static final Logger logger  = LoggerFactory.getLogger(OrganizationController.class);
 
@@ -56,26 +52,9 @@ public class OrganizationController  extends BaseController<OrganizationVO>{
 	@GetMapping("")
 	public String getUser(Model model, @RequestParam(value="index") String index){
 		model.addAttribute("index", index);
-		return "digitalplan/organization_list";
+		return "basicinfo/organization_list";
 	}
 
-//	/**
-//	 * 根据条件获取预案信息
-//	 */
-//	@ApiOperation(value="根据条件获取预案信息",notes="列表信息")
-//	@ApiImplicitParam(name="vo",value="预案信息对象")
-//	@PostMapping("/findByVO")
-//	public @ResponseBody ResultVO findByVO(@RequestBody OrganizationVO organizationVO){
-//		ResultVO resultVO = ResultVO.build();
-//		try{
-//			List<OrganizationVO> result = organizationService.doFindOrganizationlist(organizationVO);
-//			resultVO.setResult(result);
-//		}catch(Exception e){
-//			logger.error("{}",e.getMessage());
-//			resultVO.setCode(EConstants.CODE.FAILURE);
-//		}
-//		return resultVO;
-//	}
 
 	/**
 	 * 根据id获取预案信息
