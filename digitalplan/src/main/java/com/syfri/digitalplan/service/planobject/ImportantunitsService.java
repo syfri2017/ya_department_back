a73e7301753d0941f4231ebac8f681f13f716d27
huang-rui <hr18904047625@163.com>
2018-05-02 19:41:41 +0800

    重点单位

---
+++ digitalplan/src/main/java/com/syfri/digitalplan/service/planobject/ImportantunitsService.java
@@ -0,0 +1,7 @@
+package com.syfri.digitalplan.service.planobject;
+
+import com.syfri.baseapi.service.BaseService;
+import com.syfri.digitalplan.model.planobject.ImportantunitsVO;
+
+public interface ImportantunitsService  extends BaseService<ImportantunitsVO>{
+}
