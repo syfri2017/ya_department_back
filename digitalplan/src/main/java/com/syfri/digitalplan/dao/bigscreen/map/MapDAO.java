63bc3e9a4365d9cbeacaa548a7c35f9ad1c4bbd3
JC2177985 <14690588@qq.com>
2018-05-23 12:19:03 +0800

    mapbigscreen

---
+++ digitalplan/src/main/java/com/syfri/digitalplan/dao/bigscreen/map/MapDAO.java
@@ -0,0 +1,13 @@
+package com.syfri.digitalplan.dao.bigscreen.map;
+
+import com.syfri.baseapi.dao.BaseDAO;
+import com.syfri.digitalplan.model.bigscreen.map.MapVO;
+
+import java.util.List;
+
+public interface MapDAO extends BaseDAO<MapVO>{
+
+    public List<MapVO> getMapByVO(MapVO vo);
+    public List<MapVO> getShiMapByVO(MapVO vo);
+
+}
---
+++ digitalplan/src/main/java/com/syfri/digitalplan/service/bigscreen/map/MapService.java
@@ -0,0 +1,12 @@
+package com.syfri.digitalplan.service.bigscreen.map;
+
+import com.syfri.baseapi.service.BaseService;
+import com.syfri.digitalplan.model.bigscreen.map.MapVO;
+
+import java.util.List;
+
+public interface MapService  extends BaseService<MapVO>{
+
+    public List<MapVO> getMapByVO(MapVO vo);
+    public List<MapVO> getShiMapByVO(MapVO vo);
+}
