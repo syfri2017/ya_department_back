e9f7dd6977e58354accc182b9bb059a1ef028d12
rliu666 <rliu666@sina.cn>
2018-06-11 16:47:55 +0800

    车辆装载信息

---
+++ digitalplan/src/main/java/com/syfri/digitalplan/service/basicinfo/equipmentsource/EquipengineService.java
@@ -0,0 +1,7 @@
+package com.syfri.digitalplan.service.basicinfo.equipmentsource;
+
+import com.syfri.baseapi.service.BaseService;
+import com.syfri.digitalplan.model.basicinfo.equipmentsource.EquipengineVO;
+
+public interface EquipengineService  extends BaseService<EquipengineVO>{
+}
