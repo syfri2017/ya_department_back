98de653bc259543dcd9f92442d7998dc1bacd032
1106612528 <1106612528@qq.com>
2019-01-09 14:55:52 +0800

    添加二维标绘

--- digitalplan/src/main/java/com/syfri/digitalplan/dao/basicinfo/fireenginesource/FireengineDAO.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/dao/basicinfo/fireenginesource/FireengineDAO.java
@@ -2,6 +2,16 @@ package com.syfri.digitalplan.dao.basicinfo.fireenginesource;
 
 import com.syfri.baseapi.dao.BaseDAO;
 import com.syfri.digitalplan.model.basicinfo.fireenginesource.FireengineVO;
+import java.util.List;
 
 public interface FireengineDAO extends BaseDAO<FireengineVO>{
+    /**
+     * @Description:
+     * @Param:
+     * @Return:
+     * @Author: liurui
+     * @Modified By:
+     * @Date: 2018/9/21 15:15
+     */
+    public List<FireengineVO> doFindListByGis(FireengineVO fireengineVO);
 }
--- digitalplan/src/main/java/com/syfri/digitalplan/service/planobject/ImportantunitsService.java
+++ digitalplan/src/main/java/com/syfri/digitalplan/service/planobject/ImportantunitsService.java
@@ -1,6 +1,7 @@
 package com.syfri.digitalplan.service.planobject;
 
 import com.syfri.baseapi.service.BaseService;
+import com.syfri.digitalplan.model.basicinfo.fireenginesource.FireengineVO;
 import com.syfri.digitalplan.model.buildingzoning.BuildingVO;
 import com.syfri.digitalplan.model.importantparts.ImportantpartsVO;
 import com.syfri.digitalplan.model.planobject.XiaofangliliangVO;
@@ -32,4 +33,15 @@ public interface ImportantunitsService  extends BaseService<ImportantunitsVO>{
 
     /*--根据重点单位ID查询单位建筑信息详情 by li.xue 2018/8/16--*/
     List<BuildingVO> doFindJzxxDetailByZddwId(String zddwid);
+    /**
+     * 通过重点单位Gis坐标 查询周围1公里水源信息
+     * author liurui
+     */
+    Map<String, List> doFindXfsyListByZddwGis(ImportantunitsVO vo);
+
+    /**
+     * 通过重点单位Gis坐标 查询周围1公里车辆信息
+     * author liurui
+     */
+    List<FireengineVO> doFindXfclListByZddwGis(ImportantunitsVO vo);
 }
